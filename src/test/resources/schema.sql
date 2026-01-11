446601e311ab9f57e094587bbfcb4d48332c6599
KangJuSeong <wntjd3686@gmail.com>
2026-01-11 16:14:53 +0900

    test: test song like top 10 and range 1 hour

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -104,3 +104,12 @@ CREATE TABLE IF NOT EXISTS similar_songs (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS song_likes (
+    song_like_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    song_id BIGINT,
+    created_at DATETIME,
+    CONSTRAINT fk_sl_song FOREIGN KEY (song_id) REFERENCES songs(song_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
